12f4f48588b01fe280ec7d1f4bdd7f1395519850
nplm <kozma.prutkof@gmail.com>
2016-03-31 19:45:13 +0300

    Create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,10 @@
 public class Calculate {
-	public static void main(String args[]) {
+	public static void main(String a[]) {
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(a[0]);
+		int second = Integer.valueOf(a[1]);
+		int summ = first + second;
+		System.out.println("Summ " + summ);
 
 	}
 }
